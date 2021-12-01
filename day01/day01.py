ab7d7af34e75bd0535c324de33eab130fb930870
Cheryl Danner <sheromon@users.noreply.github.com>
2021-12-01 00:25:16 -0500

    Try to be at least a little bit clever, and stop relying on numpy!

--- day01/day01.py
+++ day01/day01.py
@@ -1,15 +1,13 @@
-import numpy as np
 
 
 def day01a(input_path):
+    """Find the number of times the value increases."""
     with open(input_path) as file_obj:
         depth_list = [int(line.strip()) for line in file_obj]
-    prev_val = np.inf
     counter = 0
-    for val in depth_list:
-        if val > prev_val:
+    for ind in range(0, len(depth_list) - 1):
+        if depth_list[ind + 1] > depth_list[ind]:
             counter += 1
-        prev_val = val
     return counter
 
 
@@ -18,20 +16,16 @@ def test01a():
 
 
 def day01b(input_path):
+    """Find the number of times the sum of three consecutive values increases."""
     with open(input_path) as file_obj:
         depth_list = [int(line.strip()) for line in file_obj]
-    prev_val = np.inf
-    depth_array = np.array([
-        depth_list + 2 * [0],
-        [0] + depth_list + [0],
-        2 * [0] + depth_list,
-    ])
-    sums = np.sum(depth_array, axis=0)
     counter = 0
-    for val in sums[2:-2]:
-        if val > prev_val:
+    for ind in range(0, len(depth_list) - 3):
+        # the sum of three consecutive values increases when the newest value to
+        # enter the sliding window is more than the one that is leaving, so we
+        # don't actually need to sum, I think
+        if depth_list[ind + 3] > depth_list[ind]:
             counter += 1
-        prev_val = val
     return counter
 
 
