02f6870194a1bfe2817ac8f1e975ac3b7ec4e377
Cheryl Danner <sheromon@users.noreply.github.com>
2021-12-05 00:47:36 -0500

    Day 5 solution, slightly less cheesy

--- day05/day05.py
+++ day05/day05.py
@@ -1,30 +1,27 @@
 import numpy as np
 
 
-def day05(input_path, part_b=False):
+def day05(input_path, part_a=True):
+    """Count the overlappint points for all valid lines.
+
+    For part a, only horizontal and vertical lines are valid. For part b,
+    45-degree diagonal lines are valid as well.
+    """
     points1, points2 = load_input(input_path)
     max_vals = np.max(np.vstack([points1, points2]), axis=0)
     grid = np.zeros((max_vals + 1), dtype=np.int)
 
     for p1, p2 in zip(points1, points2):
-        if p1[1] == p2[1]:
-            # being lazy here and not caring whether p1[0] or p2[0] is larger
-            # because only one of these should work, depending. actually, this
-            # would probably fail if p1 == p2, but I guess that never happens?
-            grid[p1[1], p1[0]:p2[0]+1] += 1
-            grid[p1[1], p2[0]:p1[0]+1] += 1
-        elif p1[0] == p2[0]:
-            # ditto lazy trick here
-            grid[p1[1]:p2[1]+1, p1[0]] += 1
-            grid[p2[1]:p1[1]+1, p1[0]] += 1
-        elif part_b:
-            # handle diagonal lines for part b only
-            delta = p2 - p1
-            if np.abs(delta[0]) == np.abs(delta[1]):
-                for ind in range(np.abs(delta[0]) + 1):
-                    x_ind = p1[0] + ind * np.sign(delta[0])
-                    y_ind = p1[1] + ind * np.sign(delta[1])
-                    grid[y_ind, x_ind] += 1
+        if (p1[0] != p2[0]) and (p1[1] != p2[1]) and part_a:
+            continue
+        # we should probably check to verify that any diagonal lines are
+        # 45-degree diagonal lines, but I guess they all are, because I got the
+        # right answer without checking.
+        delta = p2 - p1
+        for ind in range(np.max(np.abs(delta)) + 1):
+            x_ind = p1[0] + ind * np.sign(delta[0])
+            y_ind = p1[1] + ind * np.sign(delta[1])
+            grid[y_ind, x_ind] += 1
     return np.sum(grid >= 2)
 
 
@@ -51,7 +48,7 @@ def test05a():
 
 
 def day05b(input_path):
-    return day05(input_path, part_b=True)
+    return day05(input_path, part_a=False)
 
 
 def test05b():
