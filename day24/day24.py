1bd9a02285e60227db66f9f62cf99f29b003b16e
Cheryl Danner <sheromon@users.noreply.github.com>
2021-12-24 09:57:45 -0500

    Day 24, the approach I actually used to solve the problem

--- day24/day24.py
+++ day24/day24.py
@@ -1,73 +1,119 @@
-import collections
+import itertools
 
 import numpy as np
 
+from alu import Alu
+
 
 def load_input(input_path):
-    instruction_list = []
+    instructs = []
     with open(input_path) as file_obj:
         for line in file_obj:
-            instruction_list.append(line.strip().split())
-    return instruction_list
+            instructs.append(line.strip().split())
+    return instructs
 
 
-def day24a(input_path):
-    instruction_list = load_input(input_path)
-    # input_str = '13579246899999'
-    # int_list = [int(char) for char in input_str[::-1]]
-    while True:
-        int_list = np.random.randint(1, 10, size=14)
-        Alu(instruction_list, list(int_list)).run()
+def day24(input_path):
+    instructs = load_input(input_path)
+
+    # by stepping through the calculations, I'd figured out that there are a
+    # bunch of equality statements that are important, so start by finding those
+    eql_inds = np.array([ind for ind, line in enumerate(instructs) if line == ['eql', 'x',  'w'] ])
+
+    # I'd also figured out that some of the statements depend on the inputs and
+    # some don't, so let's run the ALU a bunch of times on random inputs to see
+    # which results are always the same and which vary
+    constants = run_random_trials(instructs, num_trials=500)
+
+    eql_check_outputs = constants[eql_inds]
+    variable_check_inds = eql_check_outputs == -1
+    print('All check outputs:', eql_check_outputs)
+    print('# constant check outputs:', np.sum(~variable_check_inds))
+    print('# variable check outputs:', np.sum(variable_check_inds))
+
+    # for the equality check outputs that vary based on the input, try running
+    # the ALU with those bits hardcoded to all combinations of True/False.
+    # there are only 7 that vary, so the search space is 2^7.
+    search_test_bit_space(instructs, np.copy(constants), eql_inds[variable_check_inds])
+
+    # based on the previous step, I know that I want all 7 checks to evaluate to
+    # True in order to get z to be 0, so generate the string expressions for w
+    # and x at each of the 7 important equality checks.
+    prev_num_steps = None
+    # doesn't matter what input is used for this step, so use the example input
+    input_str = '13579246899999'
+    for num_steps in eql_inds[variable_check_inds]:
+        # after calculating the first expression, hardcode the previous results
+        # as if each one had the condition met because we already know we need
+        # to satisfy the condition, and it makes the expression much simpler
+        if prev_num_steps is not None:
+            constants[prev_num_steps] = 1
+            constants[prev_num_steps + 1] = 0
+        alu = Alu(instructs, input_str, constants=constants, gen_expressions=True)
+        alu.run(max_steps=num_steps)
+        print('    Instruction', num_steps)
+        print('w:', alu.exprs['w'])
+        print('x:', alu.exprs['x'])
+        prev_num_steps = num_steps
+
+    # the printouts from this step give inreasingly complicated expressions, but
+    # they aren't too complicated to simplify by hand and come up with the
+    # following rules:
+    #   ints[2] = ints[3]
+    #   ints[4] = ints[5] - 2
+    #   ints[1] = ints[6] - 6
+    #   ints[10] = ints[9] - 3
+    #   ints[8] = ints[11] - 7
+    #   ints[12] = ints[7] - 8
+    #   ints[13] = ints[0] - 7
+    # then I can apply the rules to come up with the largest and smallest
+    # valid model numbers
+    input_str = '93997999296912'  # max valid model number
+    input_str = '81111379141811'  # min valid model number
+    alu = Alu(instructs, input_str, constants=constants).run()
+    print(alu.vals)
 
 
-class Alu:
+def run_random_trials(instructs, num_trials=500):
+    """Run the ALU with random inputs and bunch of times and check which
+    intermediate results are always the same and which vary.
+    """
+    all_inputs = np.random.randint(1, 9, size=(num_trials, 14))
+    # keep track of all intermediate values across multiple trials
+    all_intermeds = []
+    for ints in all_inputs:
+        alu = Alu(instructs, list(ints)).run()
+        all_intermeds.append(alu.intermeds)
 
-    def __init__(self, instruction_list, input_list):
-        self.instruction_list = instruction_list
-        self.input_list = input_list
-        self.iline = 0
-        self.vars = collections.defaultdict(int)
-        self.log_var = None
+    # determine which intermediate results are the same regardless of the input
+    all_intermeds = np.array(all_intermeds)
+    max_val = np.max(all_intermeds, axis=0)
+    is_constant = np.all(all_intermeds == max_val, axis=0)
+    print('Num constants:', np.sum(is_constant))
+    # make a list of all intermediate results with -1 for non-constant values
+    # and the value filled in for constants
+    constants = -1 * np.ones_like(is_constant)
+    constants[is_constant] = all_intermeds[0, is_constant]
 
-    def run(self):
-        for _ in self.instruction_list:
-            self.step()
-        if self.vars['z'] == 0:
-            input_str = ''.join([str(val) for val in self.input_list[::-1]])
-            print(input_str, 'is valid')
-
-    def step(self):
-        instruction = self.instruction_list[self.iline]
-        op = instruction[0]
-        name = instruction[1]
-        if op == 'inp':
-            self.vars[name] = int(self.input_list.pop())
-        else:
-            name2 = None
-            try:
-                val = int(instruction[2])
-            except:
-                name2 = instruction[2]
-                val = self.vars[instruction[2]]
-        if op == 'add':
-            self.vars[name] += val
-        elif op == 'mul':
-            self.vars[name] *= val
-        elif op == 'div':
-            self.vars[name] = self.vars[name] // val
-        elif op == 'mod':
-            self.vars[name] = self.vars[name] % val
-        elif op == 'eql':
-            self.vars[name] = int(self.vars[name] == val)
-        self.iline += 1
-        if name == self.log_var:
-            if op == 'inp':
-                print(f'Instruction {self.iline}: {op} {name} -->', self.vars[name])
-            else:
-                print(f'Instruction {self.iline}: {op} {name} {name2} ({val}) -->', self.vars[name])
+    return constants
 
 
-def test24a():
+def search_test_bit_space(instructs, constants, variable_check_inds):
+    """Force the ALU calculation to use hardcoded values at certain steps.
+
+    Using this cheat, figure out which conditions should be True or False to
+    get z to be 0 at the end.
+    """
+    input_str = '13579246899999'
+    int_list = [int(char) for char in input_str]
+    for test_bits in itertools.product(range(2), repeat=7):
+        constants[variable_check_inds] = np.array(test_bits)
+        alu = Alu(instructs, int_list, constants=constants).run()
+        if alu.vals['z'] == 0:
+            print('Found valid test bit sequence:', test_bits)
+
+
+def test24():
     assert run_test(['inp x', 'mul x -1'])['x'] == -15
     assert run_test(['inp z', 'inp x', 'mul z 3', 'eql z x'])['z'] == 1
     assert all(val == 1 for val in run_test('test_input.txt').values())
@@ -75,26 +121,15 @@ def test24a():
 
 def run_test(input_path):
     if isinstance(input_path, str):
-        instruction_list = load_input(input_path)
+        instructs = load_input(input_path)
     else:
-        instruction_list = [line.split() for line in input_path]
+        instructs = [line.split() for line in input_path]
     input_list = [15, 45]
-    input_list = input_list[::-1]
-    alu = Alu(instruction_list, list(input_list))
+    alu = Alu(instructs, list(input_list))
     alu.run()
-    return alu.vars
-
-
-def day24b(input_path):
-    pass
-
-
-def test24b():
-    assert 44169 == day24b('test_input.txt')
+    return alu.vals
 
 
 if __name__ == '__main__':
-    test24a()
-    print('Day 24a:', day24a('day24_input.txt'))
-    # test24b()
-    # print('Day 24b:', day24b('day24_input.txt'))
+    test24()
+    day24('day24_input.txt')
