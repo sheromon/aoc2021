4a5298b9e57c309246087f165573842068fd274e
Cheryl Danner <sheromon@users.noreply.github.com>
2021-12-24 09:32:56 -0500

    Refactored to use Alu class

--- day24/day24.py
+++ day24/day24.py
@@ -1,5 +1,4 @@
 import collections
-import itertools
 
 import numpy as np
 
@@ -14,59 +13,58 @@ def load_input(input_path):
 
 def day24a(input_path):
     instruction_list = load_input(input_path)
-    input_str = '13579246899999'
-    # input_str = 14 * '3'
-    int_list = [int(char) for char in input_str[::-1]]
-    # for input_vals in itertools.combinations_with_replacement(range(1, 10), 14):
+    # input_str = '13579246899999'
+    # int_list = [int(char) for char in input_str[::-1]]
     while True:
         int_list = np.random.randint(1, 10, size=14)
-        # print(int_list)
-        run_monad(instruction_list, list(int_list))
+        Alu(instruction_list, list(int_list)).run()
 
 
-def run_monad(instruction_list, input_list):
-    # print('input:', input_str)
-    # input_list = [int(char) for char in input_str[::-1]]
-    vars = collections.defaultdict(int)
-    for iline, instruction in enumerate(instruction_list):
-        process(instruction, vars, input_list, iline)
-    if vars['z'] == 0:
-        input_str = ''.join([str(val) for val in input_list[::-1]])
-        print(input_str, 'is valid')
+class Alu:
 
+    def __init__(self, instruction_list, input_list):
+        self.instruction_list = instruction_list
+        self.input_list = input_list
+        self.iline = 0
+        self.vars = collections.defaultdict(int)
+        self.log_var = None
 
-def process(instruction, vars, input_list, iline=None):
-    log_var = None
-    op = instruction[0]
-    name = instruction[1]
-    if op == 'inp':
-        vars[name] = int(input_list.pop())
-        if name == log_var:
-            print(f'input {name}:', vars[name])
-        return
-    else:
-        name2 = None
-        try:
-            val = int(instruction[2])
-        except:
-            name2 = instruction[2]
-            val = vars[instruction[2]]
-    # if iline == 6:
-    #     breakpoint()
-    if op == 'add':
-        vars[name] += val
-    elif op == 'mul':
-        vars[name] *= val
-    elif op == 'div':
-        vars[name] = vars[name] // val
-    elif op == 'mod':
-        vars[name] = vars[name] % val
-    elif op == 'eql':
-        vars[name] = int(vars[name] == val)
-        # if name == 'x' and name2 == 'w':
-        #     breakpoint()
-    if name == log_var:
-        print(f'Instruction {iline}: {op} {name} {val} ({name2}) -->', vars[name])
+    def run(self):
+        for _ in self.instruction_list:
+            self.step()
+        if self.vars['z'] == 0:
+            input_str = ''.join([str(val) for val in self.input_list[::-1]])
+            print(input_str, 'is valid')
+
+    def step(self):
+        instruction = self.instruction_list[self.iline]
+        op = instruction[0]
+        name = instruction[1]
+        if op == 'inp':
+            self.vars[name] = int(self.input_list.pop())
+        else:
+            name2 = None
+            try:
+                val = int(instruction[2])
+            except:
+                name2 = instruction[2]
+                val = self.vars[instruction[2]]
+        if op == 'add':
+            self.vars[name] += val
+        elif op == 'mul':
+            self.vars[name] *= val
+        elif op == 'div':
+            self.vars[name] = self.vars[name] // val
+        elif op == 'mod':
+            self.vars[name] = self.vars[name] % val
+        elif op == 'eql':
+            self.vars[name] = int(self.vars[name] == val)
+        self.iline += 1
+        if name == self.log_var:
+            if op == 'inp':
+                print(f'Instruction {self.iline}: {op} {name} -->', self.vars[name])
+            else:
+                print(f'Instruction {self.iline}: {op} {name} {name2} ({val}) -->', self.vars[name])
 
 
 def test24a():
@@ -80,12 +78,11 @@ def run_test(input_path):
         instruction_list = load_input(input_path)
     else:
         instruction_list = [line.split() for line in input_path]
-    vars = collections.defaultdict(int)
     input_list = [15, 45]
     input_list = input_list[::-1]
-    for instruction in instruction_list:
-        process(instruction, vars, input_list)
-    return vars
+    alu = Alu(instruction_list, list(input_list))
+    alu.run()
+    return alu.vars
 
 
 def day24b(input_path):
@@ -97,7 +94,7 @@ def test24b():
 
 
 if __name__ == '__main__':
-    # test24a()
+    test24a()
     print('Day 24a:', day24a('day24_input.txt'))
     # test24b()
     # print('Day 24b:', day24b('day24_input.txt'))
